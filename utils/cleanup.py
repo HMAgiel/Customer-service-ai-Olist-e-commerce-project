169b8016af09c6f0936d7aad1bf9bb338c6f885c
Hasyim <hasyimagiel@gmail.com>
2026-06-12 14:01:51 +0700

    stode qdrant and data clean up

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -28,17 +28,19 @@ def normalize(data):
 
 #Normalisasikan geolocation untuk referensi
 data[7]['geolocation_city'] = data[7]['geolocation_city'].apply(normalize)
-data[7].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_geolocation_dataset.csv', index=False) #Simpan sebagai csv
-#ubah ke dictionary untuk mapping file lain
-loc_fix = pd.read_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_geolocation_dataset_clean.csv')
-lokasi_zip = loc_fix.drop(columns=['geolocation_lat', 'geolocation_lng', 'geolocation_state']).set_index('geolocation_zip_code_prefix')['geolocation_city'].to_dict()
+data[7].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_geolocation_dataset_clean.csv', index=False) #Simpan sebagai csv
+
+#Bersihkan data cutomer
+data[3]['customer_city'] = data[3]['customer_city'].apply(normalize)
+data[3].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_customer_dataset_clean.csv', index=False)
 
 #Bersihkan data seller dan isi yang kosong dan aneh
-data_seller = data[4].copy()
-data_seller['seller_city_fix'] = data_seller['seller_zip_code_prefix'].map(lokasi_zip)
-data_seller['seller_city'] = data_seller['seller_city_fix'].combine_first(data_seller['seller_city'])['seller_city'] = data_seller['seller_city_fix'].combine_first(data_seller['seller_city'])
-data_seller2 = data_seller.drop(columns=["seller_city_fix"])
-data_seller2.to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_sellers_dataset_celan.csv', index=False)
+data[4]['seller_city'] = data[4]['seller_city'].apply(normalize)
+data[4].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_sellers_dataset_clean.csv', index=False)
+
+#Bersihkan aksen di produk
+data[6]['product_category_name'] = data[6]['product_category_name'].apply(normalize)
+data[6].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_product_dataset_clean.csv', index=False)
 
 #Bersihkan data review dan ubah format tanggal agar standar
 kol_tanggal = ['review_creation_date', 'review_answer_timestamp']
@@ -47,3 +49,14 @@ data[0][kol_tanggal] = data[0][kol_tanggal].apply(pd.to_datetime, format='%Y-%m-
 for i in kol_review:
     data[0][i] = data[0][i].apply(normalize)
 data[0].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_order_reviews_dataset_clean.csv', index=False)
+
+data[1]['shipping_limit_date'] = data[1]['shipping_limit_date'].apply(pd.to_datetime, format='%Y-%m-%d %H:%M:%S')
+data[1].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_order_items_dataset_clean.csv', index=False)
+
+data[2]['product_category_name'] = data[2]['product_category_name'].apply(normalize)
+data[2].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/product_category_name_translation_clean.csv', index=False)
+
+#data order
+kol_waktu_order = ['order_purchase_timestamp', 'order_approved_at', 'order_delivered_carrier_date', 'order_delivered_customer_date', 'order_estimated_delivery_date']
+data[5][kol_waktu_order] = data[5][kol_waktu_order].apply(pd.to_datetime, format='%Y-%m-%d %H:%M:%S')
+data[5].to_csv('/home/hasyim/final_project/Repo-Final-Project-Kelompok-3/data/process/olist_orders_dataset_clean.csv', index=False)
